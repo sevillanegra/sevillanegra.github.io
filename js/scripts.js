edf726c03191323388b8ae35cd99c7774375ea8c
sevillanegra <99172997+sevillanegra@users.noreply.github.com>
2022-02-17 15:57:54 +0100

    Include accordion js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,22 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
